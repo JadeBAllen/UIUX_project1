8dce32a456a01336b6917fcf97bd1bc3ed92f4d7
Jade <jade@gmail.com>
2023-05-28 16:49:46 -0600

    Completed GPT3 app

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import "./blog.css";
+import { Article } from "../../components";
+import {Rectangle01, Rectangle02, Rectangle03, Rectangle04, Rectangle05 } from "./imports";
 
 const Blog = () => {
   return (
-    <div>
-      
+    <div className="gpt3__blg section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={Rectangle01} date="Sep 26, 2021" title="GPT-3 and Open AI is the future. Let us explores how is?"/>
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article imgUrl={Rectangle02} date="Sep 26, 2021" title="GPT-3 and Open AI is the future. Let us explores how is?"/>
+          <Article imgUrl={Rectangle03} date="Sep 26, 2021" title="GPT-3 and Open AI is the future. Let us explores how is?"/>
+          <Article imgUrl={Rectangle04} date="Sep 26, 2021" title="GPT-3 and Open AI is the future. Let us explores how is?"/>
+          <Article imgUrl={Rectangle05} date="Sep 26, 2021" title="GPT-3 and Open AI is the future. Let us explores how is?"/>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,13 @@
+import Rectangle01 from "../../assets/Rectangle 22-1.png";
+import Rectangle02 from "../../assets/Rectangle 22-2.png";
+import Rectangle03 from "../../assets/Rectangle 22-3.png";
+import Rectangle04 from "../../assets/Rectangle 22-4.png";
+import Rectangle05 from "../../assets/Rectangle 22.png";
+
+export {
+    Rectangle01,
+    Rectangle02,
+    Rectangle03,
+    Rectangle04,
+    Rectangle05,
+};
